3654e21817f9d1d96dc790ac471821d34373ce56
김유민 <doumeano0o@gmail.com>
2022-07-12 00:39:10 +0900

    레이아웃 템플릿 적용, 질문등록화면 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,5 +31,10 @@ public class QuestionController {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
+	}
+	
+	@GetMapping("/create")
+	public String questionCreate() {
+		return "question_form";
 	}
 }
