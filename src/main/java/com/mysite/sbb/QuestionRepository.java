f3b417ed56a2009032878da2dd8a378ab995df24
김유민 <doumeano0o@gmail.com>
2022-07-11 19:13:21 +0900

    SELECT JUnit 연습

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,7 +1,11 @@
 package com.mysite.sbb;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{	// table에 접근하기 위한 인터페이스, 대상이 되는 Entity의 타입과 PK의 속성 타입 지정
-
+	Question findBySubject(String subject);								// subject 필드 내용으로 데이터 조회하기 위한 메서드
+	Question findBySubjectAndContent(String subject, String content);	// 제목 and 내용 조건
+	List<Question> findBySubjectLike(String subject);					// 제목 like 조건
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,6 +1,10 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +18,10 @@ class SbbApplicationTests {
 	
 	@Test			// Test 메서드임을 나타냄, Junit 실행시 호출되는 메소드
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);			// QuestionRepository 사용하여 db에 값 저장
-
-		Question q2 = new Question();
-		q2.setSubject("sbb가 무엇인가요?");
-		q2.setContent("sbb에 대해서 알고 싶습니다.");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		List<Question> q = this.questionRepository.findBySubjectLike("sbb%");
+		System.out.println(q.get(0).getId());
+		assertEquals(1, q.get(0).getId());
+		
 	}
 
 }
