1d69d4b94fdc564ae7fe89745e5a58ad23d319d4
김유민 <doumeano0o@gmail.com>
2022-07-14 00:17:58 +0900

    Add : 회원가입과 로그인을 위한 보안처리(spring Security 적용)

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http.authorizeRequests().antMatchers("/**").permitAll()		// 모든 인증되지 않은 요청 허락
+			.and().csrf().ignoringAntMatchers("/h2-console/**")		// csrf 토큰 처리시 h2 콘솔은 예외
+			.and()
+				.headers()
+				.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));	// frame 구조인 콘텐츠처리
+		return http.build();
+	}
+}
