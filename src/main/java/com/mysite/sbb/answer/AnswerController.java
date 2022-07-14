069909b51e9a98f69a1a417739d61e8c7acc741a
김유민 <doumeano0o@gmail.com>
2022-07-15 01:11:10 +0900

    Fix : 답변 등록, 수정, 추천 시 앵커 기능 추가

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -44,8 +44,8 @@ public class AnswerController {
 		}
 		
 		// 답변 등록
-		this.answerService.create(question, answerForm.getContent(), siteUser);
-		return String.format("redirect:/question/detail/%s", id);
+		Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
+		return String.format("redirect:/question/detail/%s#answer_%s", id, answer.getId());
 	}
 	
 	@PreAuthorize("isAuthenticated()")
@@ -74,7 +74,7 @@ public class AnswerController {
 		
 		// 수정
 		this.answerService.modify(answer, answerForm.getContent());
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 	
 	@PreAuthorize("isAuthenticated()")
@@ -97,6 +97,6 @@ public class AnswerController {
 		SiteUser siteUser = this.userService.getUser(principal.getName());		
 		this.answerService.vote(answer, siteUser);
 		
-		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 }
