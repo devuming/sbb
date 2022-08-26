7a1b6351408b769be5b6287c5512634d3d0cfa49
devum <doumeano0o@gmail.com>
2022-08-26 18:00:45 +0900

    -

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -2,8 +2,10 @@ package com.mysite.sbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:application-oauth.properties" })
 public class SbbApplication {
 
 	public static void main(String[] args) {
